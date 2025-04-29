a6baf2d535414ce3446ee81a7ed478fe0223203f
sasithaudayanga <sasitha.rathnayake@gmail.com>
2025-04-29 15:31:45 +0530

    day 10

---
+++ controllers/userController.js
@@ -0,0 +1,99 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export function createUser(req,res){
+    if(req.body.role == "admin"){
+        if(req.user!= null){
+            if(req.user.role != "admin"){
+                res.status(403).json({
+                    message : "You are not authorized to create an admin accounts"
+                })
+                return
+            }
+        }else{
+            res.status(403).json({
+                message : "You are not authorized to create an admin accounts. Please login first"
+            })
+            
+            return
+        }
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
+    const user = new User({
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        password : hashedPassword,
+        role : req.body.role,
+    })
+
+
+    user.save().then(
+        ()=>{
+            res.json({
+                message : "User created successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message : "Failed to create user"
+            })
+        }
+    )
+}
+
+export function loginUser(req,res){
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({email : email}).then(
+        (user)=>{
+            if(user == null){
+                res.status(404).json({
+                    message : "User not found"
+                })
+            }else{
+                const isPasswordCorrect = bcrypt.compareSync(password , user.password)
+                if(isPasswordCorrect){
+                    const token = jwt.sign(
+                        {
+                            email : user.email,
+                            firstName : user.firstName,
+                            lastName : user.lastName,
+                            role : user.role,
+                            img : user.img
+                        },
+                        "cbc-batch-five#@2025"
+                    )
+
+
+                    res.json({
+                        message : "Login successful",
+                        token : token
+                    })
+
+                }else{
+                    res.status(401).json({
+                        message : "Invalid password"
+                    })
+                }
+            }
+
+        }
+    )
+    
+}
+
+export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+    if(req.user.role != "admin"){
+        return false
+    }
+    return true
+}
---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
+import jwt from 'jsonwebtoken';
+import orderRouter from './routes/orderRoute.js';
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use(
+    (req, res, next) => {
+        const tokenString = req.header("Authorization")
+        if (tokenString != null) {
+            const token = tokenString.replace("Bearer ", "")
+
+            jwt.verify(token, "cbc-batch-five#@2025",
+                (err, decoded) => {
+                    if (decoded != null) {
+                        req.user = decoded
+                        next()
+                    } else {
+                        console.log("invalid token")
+                        res.status(403).json({
+                            message: "Invalid token"
+                        })
+                    }
+                }
+            )
+
+        } else {
+            next()
+        }
+    }
+)
+
+mongoose.connect("mongodb+srv://admin:admin123@cluster2.0wlbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2")
+    .then(() => {
+        console.log("Connected to the database")
+    }).catch(() => {
+        console.log("Database connection failed")
+    })
+
+
+
+
+app.use("/products", productRouter)
+app.use("/users", userRouter)
+app.use("/orders",orderRouter)
+
+
+app.listen(3000,
+    () => {
+        console.log('Server is running on port 3000');
+    }
+)
