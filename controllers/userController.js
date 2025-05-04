76bff3b9873b8a48e00268b8627413e65c5f68ce
Sasitha <sasitha.rathnayake@gmail.com>
2025-05-04 11:27:38 +0530

    env implemantation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 export function createUser(req,res){
     if(req.body.role == "admin"){
@@ -67,7 +71,7 @@ export function loginUser(req,res){
                             role : user.role,
                             img : user.img
                         },
-                        "cbc-batch-five#@2025"
+                        process.env.JWT_KEY
                     )
 
 
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
         if (tokenString != null) {
             const token = tokenString.replace("Bearer ", "")
 
-            jwt.verify(token, "cbc-batch-five#@2025",
+            jwt.verify(token, process.env.JWT_KEY,
                 (err, decoded) => {
                     if (decoded != null) {
                         req.user = decoded
